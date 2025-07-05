f90d8aa8086b5806d495a4f2c4acba142f916120
zwh-kx <3288969769@qq.com>
2025-07-05 16:43:59 +0800

    Add files via upload
    加了最长白列

--- 总工程/user/src/main.c
+++ 总工程/user/src/main.c
@@ -24,7 +24,8 @@
 #define ENCODER_2_B                 (TIM4_ENCODER_CH2_B7)
 
 #define PIT                         (TIM6_PIT )                                 // 使用的周期中断编号 如果修改 需要同步对应修改周期中断编号与 isr.c 中的调用
-
+#define start_column                    0	
+#define end_column                      187
 
 float putinL;
 float putinR;
@@ -36,6 +37,18 @@ uint8    W;
 uint8    H;
 float putoutline;
 float putinline;
+
+uint8 White_Column[MT9V03X_W];
+uint8 Longest_White_Column_Left[2] ={0};
+uint8 Longest_White_Column_Right[2] ={0};
+uint8 Search_Stop_Line;
+uint8 left_border;
+uint8 right_border;
+uint8 Left_Line [MT9V03X_H];
+uint8 Right_Line [MT9V03X_H];
+uint8 image_deal[MT9V03X_H][MT9V03X_W];
+uint8 Left_Lost_Flag[MT9V03X_H];
+uint8 Right_Lost_Flag[MT9V03X_H];
 
 
 int main (void)
@@ -86,9 +99,88 @@ int main (void)
 					} 
 					mt9v03x_finish_flag = 0;
 				}
-			
+				
+							//统计白线长度
+		for (W =start_column; W<=end_column; W++)
+		{
+				for (H = MT9V03X_H - 1; H >= 0; H--)
+				{
+						if(image_deal[H][W] == 0)
+								break;
+						else
+								White_Column[W]++;
+				}
+		}
+		//从左到右找左边最长白列
+    for(H=start_column;H<=end_column;H++)
+    {
+        if (Longest_White_Column_Left[0] < White_Column[H])//找最长的那一列
+        {
+            Longest_White_Column_Left[0] = White_Column[H];//【0】是白列长度
+            Longest_White_Column_Left[1] = H;              //【1】是下标，第j列
+        }
     }
+    //从右到左找右左边最长白列
+    for(H=end_column;H>=start_column;H--)//从右往左，注意条件，找到左边最长白列位置就可以停了
+    {
+        if (Longest_White_Column_Right[0] < White_Column[H])//找最长的那一列
+        {
+            Longest_White_Column_Right[0] = White_Column[H];//【0】是白列长度
+            Longest_White_Column_Right[1] = H;              //【1】是下标，第j列
+        }
+    }
+		Search_Stop_Line = Longest_White_Column_Left[0];//搜索截止行选取左或者右区别不大，他们两个理论上是一样的
+		   //相关数据使用前清零
+    Longest_White_Column_Left[0] = 0;//最长白列,[0]是最长白列的长度，[1】是第某列
+    Longest_White_Column_Left[1] = 0;//最长白列,[0]是最长白列的长度，[1】是第某列
+    Longest_White_Column_Right[0] = 0;//最长白列,[0]是最长白列的长度，[1】是第某列
+    Longest_White_Column_Right[1] = 0;//最长白列,[0]是最长白列的长度，[1】是第某列
+    for (H = MT9V03X_H - 1; H >=MT9V03X_H-Search_Stop_Line; H--)
+    {//从最下面一行，访问到有效视野行
+        for (W = Longest_White_Column_Right[1]; W <= MT9V03X_W - 1 - 2; W++)
+        {
+            if (image_deal[H][W] ==1 && image_deal[H][W + 1] == 0 && image_deal[H][W + 2] == 0)//白黑黑，找到右边界
+            {
+                right_border = W;
+                Right_Lost_Flag[H] = 0; //右丢线数组，丢线置1，不丢线置0
+                break;
+            }
+            else if(W>=MT9V03X_W-1-2)//没找到右边界，把屏幕最右赋值给右边界
+            {
+                right_border = W;
+                Right_Lost_Flag[H] = 1; //右丢线数组，丢线置1，不丢线置0
+                break;
+            }
+        }
+        for (W = Longest_White_Column_Left[1]; W >= 0 + 2; W--)//往左边扫描
+        {//从最下面一行，访问到我的有效是视野行
+            if (image_deal[H][W] ==1 && image_deal[H][W - 1] == 0 && image_deal[H][W - 2] == 0)//黑黑白认为到达左边界
+            {
+                left_border = W;
+                Left_Lost_Flag[H] = 0; //左丢线数组，丢线置1，不丢线置0
+                break;
+            }
+            else if(W<=0+2)
+            {
+                left_border = W;//找到头都没找到边，就把屏幕最左右当做边界
+                Left_Lost_Flag[H] = 1; //左丢线数组，丢线置1，不丢线置0
+                break;
+            }
+        }
+        Left_Line [H] = left_border;       //左边线线数组
+        Right_Line[H] = right_border;      //右边线线数组
+    }
+			
+			
+			
+			
+			
+			
+		}
 }
+				
+				
+
 
 
 
