c36d665a631c179c766e8efb93a1acb6bbc1fb73
unknown <3288969769@qq.com>
2025-07-25 19:47:24 +0800

    忽略

--- 总工程/总工程/code/findline.h
+++
@@ -1,9 +0,0 @@
-#ifndef _findline_h_
-#define _findline_h_
-
-extern uint8 image_deal[120][188];
-
-void image_scan(void);
-
-
-#endif
--- 总工程/总工程/code/otsu.h
+++
@@ -1,9 +0,0 @@
-#ifndef _otsu_h_
-#define _otsu_h_
-
-extern uint8 image_threshold;
-
-uint8 otsuThreshold(uint8 *image, uint16 width, uint16 height);
-
-
-#endif
